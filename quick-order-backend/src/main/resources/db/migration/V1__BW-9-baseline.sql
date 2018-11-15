ec49e8abb53aa012f2895ca4df796b04cb9a3555
dorindan <gabudeandorin@yahoo.ro>
2018-11-15 08:50:53 +0200

    allign V1

--- quick-order-backend/src/main/resources/db/migration/V1__BW-9-baseline.sql
+++ quick-order-backend/src/main/resources/db/migration/V1__BW-9-baseline.sql
@@ -1,19 +1,51 @@
+--MENU ITEM TYPE
+
+CREATE TABLE menuItemType (
+	id       int primary key,
+	type     varchar(80)
+
+);
+
+
+--MENU ITEM
+
+CREATE TABLE menuItem (
+	id               int primary key,
+	menuItemTypeId   int references menuItemType(id),
+	name			       varchar(80),
+	description		   varchar(200),
+	preparationTime  int
+
+);
+
+
+--FEEDBACK TO MENU ITEM M:M
+
+CREATE TABLE feedbackmenuItem (
+	id           int primary key,
+	menuItemId   int references menuItem(id),
+	raiting 		 int,
+	message      varchar(80)
+
+
+);
+
+
 --USER
 
-
-	CREATE TABLE userQ  (
-	id              int primary key
-
+CREATE TABLE userQ  (
+	id                    int primary key,
+  feedbackMenuItemId    int references feedbackMenuItem(id)
 );
 
 --BILL
 
 CREATE TABLE bill (
-	id               int primary key,
-	voucher			 bool,
-	sale 			 int,
-	total            float8,
-	userId			 int references userQ(id)
+	id        int primary key,
+	voucher		bool,
+	sale 			int,
+	total     float8,
+	userId		int references userQ(id)
 
 );
 
@@ -21,25 +53,25 @@ CREATE TABLE bill (
 --TABLE
 
 CREATE TABLE tableQ  (
-	id               int primary key,
-	tableNr			 int,
-	seats            int,
-	windowView       bool,
-	floor			 int,
-	free			 bool
+	id          int primary key,
+	tableNr			int,
+	seats       int,
+	windowView  bool,
+	floor			  int,
+	free			  bool
 
 );
 
 --COMMAND
 
 CREATE TABLE command  (
-	id               int primary key,
+	id             int primary key,
 	commandIdUi		 varchar(200),
 	specfication	 varchar(200) ,
-	toGo			 bool,
-	status           bool,
-	billId 			 int references bill(id),
-	tableId			 int references tableQ(id)
+	toGo			     bool,
+	status         bool,
+	billId 			   int references bill(id),
+	tableId			   int references tableQ(id)
 
 );
 
@@ -47,7 +79,7 @@ CREATE TABLE command  (
 --USER COMMAND M:M
 
 CREATE TABLE userCommand (
-	id               int primary key,
+	id           int primary key,
 	userId			 int references userQ(id),
 	commandId		 int references command(id)
 
@@ -59,11 +91,11 @@ CREATE TABLE userCommand (
 	CREATE TABLE reservation  (
 	id              int primary key,
 	timeCheckIn     timestamp,
-	timeCheckOut     timestamp,
-	userId			 int references userQ(id),
-	commandId		 int references command(id),
-	confirmed        bool,
-	status			 bool
+	timeCheckOut    timestamp,
+	userId			    int references userQ(id),
+	commandId		    int references command(id),
+	confirmed       bool,
+	status			    bool
 
 
 );
@@ -72,8 +104,8 @@ CREATE TABLE userCommand (
 
 	CREATE TABLE userReservation  (
 	id               int primary key,
-	userId			 int references userQ(id),
-	reservationId	 int references reservation(id)
+	userId			     int references userQ(id),
+	reservationId	   int references reservation(id)
 
 );
 
@@ -81,39 +113,18 @@ CREATE TABLE userCommand (
 
 CREATE TABLE tableReservation  (
 	id               int primary key,
-	reservationId	 int references reservation(id),
-	tableId			 int references tableQ(id)
+	reservationId	   int references reservation(id),
+	tableId			     int references tableQ(id)
 
 );
 
-
---MENU ITEM TYPE
-
-CREATE TABLE menuItemType (
-	id               int primary key,
-	type             varchar(80)
-
-
-);
-
-
---MENU ITEM
-
-CREATE TABLE menuItem (
-	id               int primary key,
-	menuItemTypeId   int references menuItemType(id),
-	name			 varchar(80),
-	description		 varchar(200),
-	preparationTime  int
-
-);
 
 --MENU ITEM COMMAND M:M
 
 CREATE TABLE menuItemCommand (
-	id               int primary key,
+	id             int primary key,
 	menuItemId   	 int references menuItem(id),
-	commandId		 int references command(id),
+	commandId		   int references command(id),
 	statusFood		 bool
 
 
@@ -122,7 +133,7 @@ CREATE TABLE menuItemCommand (
 --INGREDIENT
 
 CREATE TABLE ingredient (
-	id               int primary key,
+	id         int primary key,
 	name 			 varchar(200)
 
 
@@ -131,7 +142,7 @@ CREATE TABLE ingredient (
 --MENU ITEM INGREDIENT M:M
 
 CREATE TABLE menuItemIngredient (
-	id               int primary key,
+	id             int primary key,
 	menuItemId   	 int references menuItem(id),
 	ingredientId	 int references ingredient(id)
 
@@ -140,15 +151,6 @@ CREATE TABLE menuItemIngredient (
 
 
 
---FEEDBACK TO MENU ITEM M:M
 
-CREATE TABLE feedbackmenuItem (
-	id               int primary key,
-	menuItemId   	 int references menuItem(id),
-	raiting 		 int,
-	message          varchar(80)
-
-
-);
 
 alter table userQ add column feedbackMenuItemId int references feedbackMenuItem(id);
