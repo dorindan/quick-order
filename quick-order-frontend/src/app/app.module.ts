90a0cc8679fd5d6f66429623773e1446e3a67e0f
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2018-12-05 13:43:04 +0200

    BW-30 add header footer startpage

--- quick-order-frontend/src/app/app.module.ts
+++ quick-order-frontend/src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './header/logo/logo.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { FooterComponent } from './footer/footer.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { StartLoggedInComponent } from './start-logged-in/start-logged-in.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent,
+    StartPageComponent,
+    StartLoggedInComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
