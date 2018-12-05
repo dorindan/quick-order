0b9ec131c70994dfab226576df6c8a6a4dcda9e2
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2018-12-05 13:43:54 +0200

    BW-30 add header footer startpage

---
+++ quick-order-frontend/src/app/start-logged-in/start-logged-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-logged-in',
+  templateUrl: './start-logged-in.component.html',
+  styleUrls: ['./start-logged-in.component.scss']
+})
+export class StartLoggedInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
