3b6e9b18ca817a1b887813697d75a6e3cc3fed0e
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2018-12-10 12:12:49 +0200

    delete logo component + change name of picture

--- quick-order-frontend/src/app/app.module.ts
+++ quick-order-frontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
-import { LogoComponent } from './header/logo/logo.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { FooterComponent } from './footer/footer.component';
 import { StartPageComponent } from './start-page/start-page.component';
@@ -17,7 +16,6 @@ import { ReservationComponent } from './reservation/reservation.component';
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    LogoComponent,
     FooterComponent,
     StartPageComponent,
     StartLoggedInComponent,
