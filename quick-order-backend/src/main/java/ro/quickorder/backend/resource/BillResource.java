e709529180cfa9636dda562e9d85c41c0513cb7d
Ganditoru <robert_eu_97@yahoo.com>
2019-01-16 15:01:31 +0200

    Connecting backend to frontend example

--- quick-order-backend/src/main/java/ro/quickorder/backend/resource/BillResource.java
+++ quick-order-backend/src/main/java/ro/quickorder/backend/resource/BillResource.java
@@ -1,20 +1,36 @@
 package ro.quickorder.backend.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ro.quickorder.backend.model.Bill;
 import ro.quickorder.backend.repository.BillRepository;
 
+import javax.ws.rs.Produces;
+import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class BillResource {
     @Autowired
     BillRepository billRepository;
 
     @RequestMapping(path = "/bill", method = RequestMethod.GET)
-    public String findById(@RequestParam(value = "id", defaultValue = "0") Long id) {
-        return billRepository.findById(id).toString();
+    @Produces("text/plain")
+    public Optional<Bill> findById(@RequestParam(value = "id", defaultValue = "0") Long id) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String valueAsString=objectMapper.writeValueAsString(billRepository.findById(id));
+            System.out.println(valueAsString);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return billRepository.findById(id);
+    }
+
+    @PostMapping(path="/bill")
+    public void createBill(@RequestBody Bill bill){
+        System.out.println(bill.getTotal());
     }
 }
--- quick-order-frontend/src/app/app.module.ts
+++ quick-order-frontend/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { StartLoggedInComponent } from './start-logged-in/start-logged-in.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ReservationComponent } from './reservation/reservation.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
