81f47bf4258415f46a6a9809f7a1ce5559f2b4fc
dorindan <gabudeandorin@yahoo.ro>
2019-03-06 15:45:11 +0200

    finish menu

--- quick-order-frontend/src/app/models/MenuItem.ts
+++ quick-order-frontend/src/app/models/MenuItem.ts
@@ -1,15 +1,27 @@
+import {Ingredient} from "./Ingredient";
+
 export class MenuItem{
+  get ingredients(): Ingredient[] {
+    return this._ingredients;
+  }
+
+  set ingredients(value: Ingredient[]) {
+    this._ingredients = value;
+  }
   private _id:number;
   private _name:string;
   private _description:string;
   private _preparationDurationInMinutes:number;
+  private _ingredients: Ingredient[];
   private _price:number;
 
-  constructor(id: number, name: string, description: string, preparationDurationInMinutes: number, price: number) {
+
+  constructor(id: number, name: string, description: string, preparationDurationInMinutes: number, ingredients: Ingredient[], price: number) {
     this._id = id;
     this._name = name;
     this._description = description;
     this._preparationDurationInMinutes = preparationDurationInMinutes;
+    this._ingredients = ingredients;
     this._price = price;
   }
 
--- quick-order-frontend/src/app/pages/menu/menu.component.ts
+++ quick-order-frontend/src/app/pages/menu/menu.component.ts
@@ -12,6 +12,7 @@ export class MenuComponent implements OnInit {
 
   menu: Menu[];
   title: string = "yay";
+
   constructor(private menuService:MenuService) { }
 
   ngOnInit() {
@@ -20,6 +21,29 @@ export class MenuComponent implements OnInit {
       this.menu = response;
     })
 
+  }
+
+  ingrediente(menuID: number, menuItemID: number): string{
+    let ingredientele: string = "gsp";
+    this.menu.forEach((menu) => {
+      if (menu.id === menuID)
+      {
+        //console.log("I'm in menu" + menu.id);
+        menu.menuItems.forEach((menuItem) => {
+          if (menuItem.id === menuItemID)
+          {
+            //console.log("I'm in menuItem" + menuItem.id);
+            menuItem.ingredients.forEach((ingredient) => {
+              //console.log("I have ingredient" + ingredient.name);
+              ingredientele.concat(ingredient.name);
+              ingredientele.concat(',');
+            })
+          }
+        })
+      }
+    })
+    ingredientele = ingredientele.substring(0, ingredientele.length - 1)
+    return ingredientele;
   }
 
 }
