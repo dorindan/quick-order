f2d6cb378c4eb68950b7e8bd631493ea9dc238a2
dorindan <gabudeandorin@yahoo.ro>
2019-03-07 17:26:19 +0200

    remove comments

--- quick-order-frontend/src/app/pages/menu/menu.component.ts
+++ quick-order-frontend/src/app/pages/menu/menu.component.ts
@@ -22,28 +22,4 @@ export class MenuComponent implements OnInit {
     })
 
   }
-
-  ingrediente(menuID: number, menuItemID: number): string{
-    let ingredientele: string = "gsp";
-    this.menu.forEach((menu) => {
-      if (menu.id === menuID)
-      {
-        //console.log("I'm in menu" + menu.id);
-        menu.menuItems.forEach((menuItem) => {
-          if (menuItem.id === menuItemID)
-          {
-            //console.log("I'm in menuItem" + menuItem.id);
-            menuItem.ingredients.forEach((ingredient) => {
-              //console.log("I have ingredient" + ingredient.name);
-              ingredientele.concat(ingredient.name);
-              ingredientele.concat(',');
-            })
-          }
-        })
-      }
-    })
-    ingredientele = ingredientele.substring(0, ingredientele.length - 1)
-    return ingredientele;
-  }
-
 }
