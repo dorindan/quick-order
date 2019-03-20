24d8be222688a5244982807b84deccfb017053c3
Robert Ganditoru <ganditorurobert@gmail.com>
2019-03-20 14:02:47 +0200

    BW-47 refactor sql script for change id type

--- quick-order-backend/src/main/resources/db/migration/V1.0.15__change_id_type_to_varchar.sql
+++ quick-order-backend/src/main/resources/db/migration/V1.0.15__change_id_type_to_varchar.sql
@@ -1,9 +1,9 @@
 ----------------------------------- Delete all foreign key  ----------------------
 -- bill
-ALTER TABLE bill ADD id_uuid varchar(40);
+ALTER TABLE bill ADD id_uuid VARCHAR(40);
 
 UPDATE bill
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE bill DROP COLUMN user_id;
 
@@ -12,10 +12,10 @@ RENAME COLUMN id_uuid TO user_id;
 
 
 -- command 1
-ALTER TABLE command ADD id_uuid varchar(40);
+ALTER TABLE command ADD id_uuid VARCHAR(40);
 
 UPDATE command
-set id_uuid = cast(table_id as varchar(40));
+SET id_uuid = CAST(table_id AS VARCHAR(40));
 
 ALTER TABLE command DROP COLUMN table_id;
 
@@ -23,10 +23,10 @@ ALTER TABLE command
 RENAME COLUMN id_uuid TO table_id;
 
 -- command 2
-ALTER TABLE command ADD id_uuid varchar(40);
+ALTER TABLE command ADD id_uuid VARCHAR(40);
 
 UPDATE command
-set id_uuid = cast(bill_id as varchar(40));
+SET id_uuid = CAST(bill_id AS VARCHAR(40));
 
 ALTER TABLE command DROP COLUMN bill_id;
 
@@ -35,10 +35,10 @@ RENAME COLUMN id_uuid TO bill_id;
 
 
 -- feedback_menu_item 1
-ALTER TABLE feedback_menu_item ADD id_uuid varchar(40);
+ALTER TABLE feedback_menu_item ADD id_uuid VARCHAR(40);
 
 UPDATE feedback_menu_item
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE feedback_menu_item DROP COLUMN user_id;
 
@@ -46,10 +46,10 @@ ALTER TABLE feedback_menu_item
 RENAME COLUMN id_uuid TO user_id;
 
 -- feedback_menu_item 2
-ALTER TABLE feedback_menu_item ADD id_uuid varchar(40);
+ALTER TABLE feedback_menu_item ADD id_uuid VARCHAR(40);
 
 UPDATE feedback_menu_item
-set id_uuid = cast(menu_item_id as varchar(40));
+SET id_uuid = CAST(menu_item_id AS VARCHAR(40));
 
 ALTER TABLE feedback_menu_item DROP COLUMN menu_item_id;
 
@@ -58,10 +58,10 @@ RENAME COLUMN id_uuid TO menu_item_id;
 
 
 -- menu_item 1
-ALTER TABLE menu_item ADD id_uuid varchar(40);
+ALTER TABLE menu_item ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item
-set id_uuid = cast(menu_item_type_id as varchar(40));
+SET id_uuid = CAST(menu_item_type_id AS VARCHAR(40));
 
 ALTER TABLE menu_item DROP COLUMN menu_item_type_id;
 
@@ -70,10 +70,10 @@ RENAME COLUMN id_uuid TO menu_item_type_id;
 
 
 -- menu_item_command 1
-ALTER TABLE menu_item_command ADD id_uuid varchar(40);
+ALTER TABLE menu_item_command ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_command
-set id_uuid = cast(menu_item_id as varchar(40));
+SET id_uuid = CAST(menu_item_id AS VARCHAR(40));
 
 ALTER TABLE menu_item_command DROP COLUMN menu_item_id;
 
@@ -81,10 +81,10 @@ ALTER TABLE menu_item_command
 RENAME COLUMN id_uuid TO menu_item_id;
 
 -- menu_item_command 2
-ALTER TABLE menu_item_command ADD id_uuid varchar(40);
+ALTER TABLE menu_item_command ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_command
-set id_uuid = cast(command_id as varchar(40));
+SET id_uuid = CAST(command_id AS VARCHAR(40));
 
 ALTER TABLE menu_item_command DROP COLUMN command_id;
 
@@ -93,10 +93,10 @@ RENAME COLUMN id_uuid TO command_id;
 
 
 -- menu_item_ingredient 1
-ALTER TABLE menu_item_ingredient ADD id_uuid varchar(40);
+ALTER TABLE menu_item_ingredient ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_ingredient
-set id_uuid = cast(menu_item_id as varchar(40));
+SET id_uuid = CAST(menu_item_id AS VARCHAR(40));
 
 ALTER TABLE menu_item_ingredient DROP COLUMN menu_item_id;
 
@@ -104,10 +104,10 @@ ALTER TABLE menu_item_ingredient
 RENAME COLUMN id_uuid TO menu_item_id;
 
 -- menu_item_ingredient 2
-ALTER TABLE menu_item_ingredient ADD id_uuid varchar(40);
+ALTER TABLE menu_item_ingredient ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_ingredient
-set id_uuid = cast(ingredient_id as varchar(40));
+SET id_uuid = CAST(ingredient_id AS VARCHAR(40));
 
 ALTER TABLE menu_item_ingredient DROP COLUMN ingredient_id;
 
@@ -116,10 +116,10 @@ RENAME COLUMN id_uuid TO ingredient_id;
 
 
 -- reservation 1
-ALTER TABLE reservation ADD id_uuid varchar(40);
+ALTER TABLE reservation ADD id_uuid VARCHAR(40);
 
 UPDATE reservation
-set id_uuid = cast(command_id as varchar(40));
+SET id_uuid = CAST(command_id AS VARCHAR(40));
 
 ALTER TABLE reservation DROP COLUMN command_id;
 
@@ -127,10 +127,10 @@ ALTER TABLE reservation
 RENAME COLUMN id_uuid TO command_id;
 
 -- reservation 2
-ALTER TABLE reservation ADD id_uuid varchar(40);
+ALTER TABLE reservation ADD id_uuid VARCHAR(40);
 
 UPDATE reservation
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE reservation DROP COLUMN user_id;
 
@@ -139,10 +139,10 @@ RENAME COLUMN id_uuid TO user_id;
 
 
 -- table_reservation 1
-ALTER TABLE table_reservation ADD id_uuid varchar(40);
+ALTER TABLE table_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE table_reservation
-set id_uuid = cast(reservation_id as varchar(40));
+SET id_uuid = CAST(reservation_id AS VARCHAR(40));
 
 ALTER TABLE table_reservation DROP COLUMN reservation_id;
 
@@ -150,10 +150,10 @@ ALTER TABLE table_reservation
 RENAME COLUMN id_uuid TO reservation_id;
 
 -- table_reservation 2
-ALTER TABLE table_reservation ADD id_uuid varchar(40);
+ALTER TABLE table_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE table_reservation
-set id_uuid = cast(table_id as varchar(40));
+SET id_uuid = CAST(table_id AS VARCHAR(40));
 
 ALTER TABLE table_reservation DROP COLUMN table_id;
 
@@ -162,10 +162,10 @@ RENAME COLUMN id_uuid TO table_id;
 
 
 -- user_attribute 1
-ALTER TABLE user_attribute ADD id_uuid varchar(40);
+ALTER TABLE user_attribute ADD id_uuid VARCHAR(40);
 
 UPDATE user_attribute
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE user_attribute DROP COLUMN user_id;
 
@@ -174,10 +174,10 @@ RENAME COLUMN id_uuid TO user_id;
 
 
 -- user_command 1
-ALTER TABLE user_command ADD id_uuid varchar(40);
+ALTER TABLE user_command ADD id_uuid VARCHAR(40);
 
 UPDATE user_command
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE user_command DROP COLUMN user_id;
 
@@ -185,10 +185,10 @@ ALTER TABLE user_command
 RENAME COLUMN id_uuid TO user_id;
 
 -- user_command 2
-ALTER TABLE user_command ADD id_uuid varchar(40);
+ALTER TABLE user_command ADD id_uuid VARCHAR(40);
 
 UPDATE user_command
-set id_uuid = cast(command_id as varchar(40));
+SET id_uuid = CAST(command_id AS VARCHAR(40));
 
 ALTER TABLE user_command DROP COLUMN command_id;
 
@@ -197,10 +197,10 @@ RENAME COLUMN id_uuid TO command_id;
 
 
 -- user_reservation 1
-ALTER TABLE user_reservation ADD id_uuid varchar(40);
+ALTER TABLE user_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE user_reservation
-set id_uuid = cast(user_id as varchar(40));
+SET id_uuid = CAST(user_id AS VARCHAR(40));
 
 ALTER TABLE user_reservation DROP COLUMN user_id;
 
@@ -208,10 +208,10 @@ ALTER TABLE user_reservation
 RENAME COLUMN id_uuid TO user_id;
 
 -- user_reservation 2
-ALTER TABLE user_reservation ADD id_uuid varchar(40);
+ALTER TABLE user_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE user_reservation
-set id_uuid = cast(reservation_id as varchar(40));
+SET id_uuid = CAST(reservation_id AS VARCHAR(40));
 
 ALTER TABLE user_reservation DROP COLUMN reservation_id;
 
@@ -222,10 +222,10 @@ RENAME COLUMN id_uuid TO reservation_id;
 ---------------------------- Change primary key -------------------------------
 
 -- bill
-ALTER TABLE bill ADD id_uuid varchar(40);
+ALTER TABLE bill ADD id_uuid VARCHAR(40);
 
 UPDATE bill
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE bill DROP COLUMN id;
 
@@ -237,10 +237,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- command
-ALTER TABLE command ADD id_uuid varchar(40);
+ALTER TABLE command ADD id_uuid VARCHAR(40);
 
 UPDATE command
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE command DROP COLUMN id;
 
@@ -252,10 +252,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- feedback_menu_item
-ALTER TABLE feedback_menu_item ADD id_uuid varchar(40);
+ALTER TABLE feedback_menu_item ADD id_uuid VARCHAR(40);
 
 UPDATE feedback_menu_item
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE feedback_menu_item DROP COLUMN id;
 
@@ -267,10 +267,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- ingredient
-ALTER TABLE ingredient ADD id_uuid varchar(40);
+ALTER TABLE ingredient ADD id_uuid VARCHAR(40);
 
 UPDATE ingredient
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE ingredient DROP COLUMN id;
 
@@ -282,10 +282,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- menu_item
-ALTER TABLE menu_item ADD id_uuid varchar(40);
+ALTER TABLE menu_item ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE menu_item DROP COLUMN id;
 
@@ -297,10 +297,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- menu_item_command
-ALTER TABLE menu_item_command ADD id_uuid varchar(40);
+ALTER TABLE menu_item_command ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_command
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE menu_item_command DROP COLUMN id;
 
@@ -312,10 +312,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- menu_item_ingredient
-ALTER TABLE menu_item_ingredient ADD id_uuid varchar(40);
+ALTER TABLE menu_item_ingredient ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_ingredient
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE menu_item_ingredient DROP COLUMN id;
 
@@ -327,10 +327,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- menu_item_type
-ALTER TABLE menu_item_type ADD id_uuid varchar(40);
+ALTER TABLE menu_item_type ADD id_uuid VARCHAR(40);
 
 UPDATE menu_item_type
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE menu_item_type DROP COLUMN id;
 
@@ -342,10 +342,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- reservation
-ALTER TABLE reservation ADD id_uuid varchar(40);
+ALTER TABLE reservation ADD id_uuid VARCHAR(40);
 
 UPDATE reservation
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE reservation DROP COLUMN id;
 
@@ -357,10 +357,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- table_food
-ALTER TABLE table_food ADD id_uuid varchar(40);
+ALTER TABLE table_food ADD id_uuid VARCHAR(40);
 
 UPDATE table_food
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE table_food DROP COLUMN id;
 
@@ -372,10 +372,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- table_reservation
-ALTER TABLE table_reservation ADD id_uuid varchar(40);
+ALTER TABLE table_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE table_reservation
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE table_reservation DROP COLUMN id;
 
@@ -387,10 +387,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- user_attribute
-ALTER TABLE user_attribute ADD id_uuid varchar(40);
+ALTER TABLE user_attribute ADD id_uuid VARCHAR(40);
 
 UPDATE user_attribute
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE user_attribute DROP COLUMN id;
 
@@ -402,10 +402,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- user_command
-ALTER TABLE user_command ADD id_uuid varchar(40);
+ALTER TABLE user_command ADD id_uuid VARCHAR(40);
 
 UPDATE user_command
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE user_command DROP COLUMN id;
 
@@ -417,10 +417,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- user_reservation
-ALTER TABLE user_reservation ADD id_uuid varchar(40);
+ALTER TABLE user_reservation ADD id_uuid VARCHAR(40);
 
 UPDATE user_reservation
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE user_reservation DROP COLUMN id;
 
@@ -432,10 +432,10 @@ RENAME COLUMN id_uuid TO id;
 
 
 -- users
-ALTER TABLE users ADD id_uuid varchar(40);
+ALTER TABLE users ADD id_uuid VARCHAR(40);
 
 UPDATE users
-set id_uuid = cast(id as varchar(40));
+SET id_uuid = CAST(id AS VARCHAR(40));
 
 ALTER TABLE users DROP COLUMN id;
 
@@ -450,55 +450,55 @@ RENAME COLUMN id_uuid TO id;
 
 
 ALTER TABLE bill
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_bill_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE command
-ADD CONSTRAINT table_food FOREIGN KEY (table_id) REFERENCES table_food (id);
+ADD CONSTRAINT FK_command_table_food FOREIGN KEY (table_id) REFERENCES table_food (id);
 
 ALTER TABLE command
-ADD CONSTRAINT bill FOREIGN KEY (bill_id) REFERENCES bill (id);
+ADD CONSTRAINT FK_command_bill FOREIGN KEY (bill_id) REFERENCES bill (id);
 
 ALTER TABLE feedback_menu_item
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_feedback_menu_item_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE menu_item
-ADD CONSTRAINT menu_item_type FOREIGN KEY (menu_item_type_id) REFERENCES menu_item_type (id);
+ADD CONSTRAINT FK_menu_item_menu_item_type FOREIGN KEY (menu_item_type_id) REFERENCES menu_item_type (id);
 
 ALTER TABLE menu_item_command
-ADD CONSTRAINT menu_item FOREIGN KEY (menu_item_id) REFERENCES menu_item (id);
+ADD CONSTRAINT FK_menu_item_command_menu_item FOREIGN KEY (menu_item_id) REFERENCES menu_item (id);
 
 ALTER TABLE menu_item_command
-ADD CONSTRAINT command FOREIGN KEY (command_id) REFERENCES command (id);
+ADD CONSTRAINT FK_menu_item_command_command FOREIGN KEY (command_id) REFERENCES command (id);
 
 ALTER TABLE menu_item_ingredient
-ADD CONSTRAINT menu_item FOREIGN KEY (menu_item_id) REFERENCES menu_item (id);
+ADD CONSTRAINT FK_menu_item_ingredient_menu_item FOREIGN KEY (menu_item_id) REFERENCES menu_item (id);
 
 ALTER TABLE menu_item_ingredient
-ADD CONSTRAINT ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient (id);
+ADD CONSTRAINT FK_menu_item_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient (id);
 
 ALTER TABLE reservation
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_reservation_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE reservation
-ADD CONSTRAINT command FOREIGN KEY (command_id) REFERENCES command (id);
+ADD CONSTRAINT FK_reservation_command FOREIGN KEY (command_id) REFERENCES command (id);
 
 ALTER TABLE table_reservation
-ADD CONSTRAINT reservation FOREIGN KEY (reservation_id) REFERENCES reservation (id);
+ADD CONSTRAINT FK_table_reservation_reservation FOREIGN KEY (reservation_id) REFERENCES reservation (id);
 
 ALTER TABLE table_reservation
-ADD CONSTRAINT table_food FOREIGN KEY (table_id) REFERENCES table_food (id);
+ADD CONSTRAINT FK_table_reservation_table_food FOREIGN KEY (table_id) REFERENCES table_food (id);
 
 ALTER TABLE user_attribute
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_user_attribute_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE user_command
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_user_command_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE user_command
-ADD CONSTRAINT command FOREIGN KEY (command_id) REFERENCES command (id);
+ADD CONSTRAINT FK_user_command_command FOREIGN KEY (command_id) REFERENCES command (id);
 
 ALTER TABLE user_reservation
-ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT FK_user_reservation_users FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE user_reservation
-ADD CONSTRAINT reservation FOREIGN KEY (reservation_id) REFERENCES reservation (id);
+ADD CONSTRAINT FK_user_reservation_reservation FOREIGN KEY (reservation_id) REFERENCES reservation (id);
