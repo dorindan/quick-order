92d4a428ccaf15fb1eb3ee7a6a8d6bbb31867f12
Robert Ganditoru <ganditorurobert@gmail.com>
2019-04-30 15:49:07 +0300

    Merge branch 'master' into feature/BW-59
    # Conflicts:
    #	quick-order-backend/src/main/java/ro/quickorder/backend/model/TableFood.java
    #	quick-order-backend/src/main/java/ro/quickorder/backend/resource/TableFoodResource.java
    #	quick-order-backend/src/main/java/ro/quickorder/backend/service/TableFoodService.java
    #	quick-order-backend/src/test/java/ro/quickorder/backend/service/ReservationServiceTest.java
    #	quick-order-backend/src/test/java/ro/quickorder/backend/service/TableFoodServiceTest.java
    #	quick-order-frontend/src/app/app.module.ts
    #	quick-order-frontend/src/assets/i18n/EN.json
    #	quick-order-frontend/src/assets/i18n/RO.json

--- quick-order-backend/src/main/java/ro/quickorder/backend/repository/TableFoodRepository.java
+++ quick-order-backend/src/main/java/ro/quickorder/backend/repository/TableFoodRepository.java
@@ -1,9 +1,12 @@
 package ro.quickorder.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.quickorder.backend.model.TableFood;
 import sun.awt.SunHints;
+
+import java.util.List;
 
 @Repository
 public interface TableFoodRepository extends JpaRepository<TableFood, Long> {
