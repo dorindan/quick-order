8b3ba503c1365a2ffd45aec29dc1a9506019df1c
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2019-07-10 15:53:46 +0300

    feature/BW-92 create default users with roles

--- quick-order-backend/src/main/resources/db/migration/V1.0.25__add_default_users_with_roles.sql
+++ quick-order-backend/src/main/resources/db/migration/V1.0.25__add_default_users_with_roles.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(username,password,email, id) VALUES('quick_user',
-'$2a$10$WLB7YVjd1t6KVJawpysv5exFt3yWQ8A.ZVHXC/b0kHRpOc7yZCgtG','quick_user@yahoo.com',1);
+'$2a$10$/TVePP2x5TTGFM.xCsnjYu8JNtQ4KT43VXbd5rP19J1e1j3z6Ge82','quickorder.user@yandex.com',1);
 INSERT INTO users(username,password,email, id) VALUES('quick_waiter',
-'$2a$10$WLB7YVjd1t6KVJawpysv5exFt3yWQ8A.ZVHXC/b0kHRpOc7yZCgtG','quick_waiter@yahoo.com',2);
+'$2a$10$/TVePP2x5TTGFM.xCsnjYu8JNtQ4KT43VXbd5rP19J1e1j3z6Ge82','quickorder.user@yandex.com',2);
 INSERT INTO user_roles(user_id,role_id) VALUES(1,1);
 INSERT INTO user_roles(user_id,role_id) VALUES(2,2);
