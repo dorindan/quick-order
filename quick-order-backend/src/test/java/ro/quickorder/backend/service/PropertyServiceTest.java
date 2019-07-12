cd68c374ecced28f0b3a8a0f81de66925c6f5a5b
dorindan <gabudeandorin@yahoo.ro>
2019-07-12 10:46:21 +0300

    obliterate property tests'

--- quick-order-backend/src/test/java/ro/quickorder/backend/service/PropertyServiceTest.java
+++ quick-order-backend/src/test/java/ro/quickorder/backend/service/PropertyServiceTest.java
@@ -31,72 +31,13 @@ public class PropertyServiceTest {
 
     @Before
     public void setUP(){
-        Property startTimeProperty = new Property(PropertyName.START_TIME, "10");
-        Property endTimeProperty = new Property(PropertyName.END_TIME, "18");
-
-        propertyRepository.save(startTimeProperty);
-        propertyRepository.save(endTimeProperty);
     }
 
     @After
     public void tearDown(){
-        propertyRepository.deleteAll();
+
     }
 
-    @Test
-    public void findScheduleTest(){
-        PropertyDto programDto = propertyService.findProperties();
 
-        Assert.assertEquals(10, programDto.getStartProgramTime().getHour());
-        Assert.assertEquals(18, programDto.getEndProgramTime().getHour());
-    }
-
-    @Test
-    public void findScheduleNoStartTimeTest(){
-        propertyRepository.deleteAll();
-
-        Property endTimeProperty = new Property(PropertyName.END_TIME, "18");
-        propertyRepository.save(endTimeProperty);
-
-        try {
-            PropertyDto programDto = propertyService.findProperties();
-            fail("There should be no start property!");
-        } catch(NullPointerException e){
-            Assert.assertEquals("Start date was not found!", e.getMessage());
-        }
-    }
-
-    @Test
-    public void findScheduleNoEndTimeTest(){
-        propertyRepository.deleteAll();
-
-        Property startTimeProperty = new Property(PropertyName.START_TIME, "18");
-        propertyRepository.save(startTimeProperty);
-
-        try {
-            PropertyDto programDto = propertyService.findProperties();
-            fail("There should be no end property!");
-        } catch(NullPointerException e){
-            Assert.assertEquals("End date was not found!", e.getMessage());
-        }
-    }
-
-    @Test
-    public void findScheduleBadValueInDatabaseTest(){
-        propertyRepository.deleteAll();
-
-        Property startTimeProperty = new Property(PropertyName.START_TIME, "10");
-        Property endTimeProperty = new Property(PropertyName.END_TIME, "18 break");
-
-        propertyRepository.save(startTimeProperty);
-        propertyRepository.save(endTimeProperty);
-
-        try {
-            PropertyDto programDto = propertyService.findProperties();
-            fail("Error should appear, one of the values is not a number!");
-        } catch(NullPointerException e){
-            Assert.assertEquals("Value is not a number!", e.getMessage());
-        }
-    }
 
 }
