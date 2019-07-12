8bd5df48204753e3c0ca10dbefdace174dfce651
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2019-07-12 12:42:21 +0300

    feature/BW-85 about us

--- quick-order-frontend/src/app/pages/about-us/about-us.component.ts
+++ quick-order-frontend/src/app/pages/about-us/about-us.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {PropertyService} from '../../services/property.service';
 
 @Component({
   selector: 'app-about-us',
@@ -8,9 +9,20 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AboutUsComponent implements OnInit {
 
-  hours = '10:00 - 20:00';
+  hours = '';
 
-  constructor(private translateService: TranslateService) {
+  constructor(private translateService: TranslateService,
+              private propertyService: PropertyService) {
+    this.propertyService.getBistroProperty().subscribe(response => {
+      const startHours: string[] = response.startProgramTime.split(':', 3);
+      const startHour = new Date();
+      startHour.setHours(+startHours[0], +startHours[1], +startHours[2]);
+      const endHours: string[] = response.endProgramTime.split(':', 3);
+      const endHour = new Date();
+      endHour.setHours(+endHours[0], +endHours[1], +endHours[2]);
+      this.hours = startHour.getHours() + ':00' + ' - '
+        + endHour.getHours() + ':00';
+    });
   }
 
   ngOnInit() {
