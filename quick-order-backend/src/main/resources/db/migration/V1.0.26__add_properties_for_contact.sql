8c2837cceafe23844c4201e924f220e7e94f8903
szidoniaszappanyos <szidoniaszappanyos@nttdata.ro>
2019-07-15 15:07:20 +0300

    feature/BW-98 add contact properties

---
+++ quick-order-backend/src/main/resources/db/migration/V1.0.26__add_properties_for_contact.sql
@@ -0,0 +1,6 @@
+INSERT INTO property(id,name,value) VALUES (3,'STREET_NAME','STR. CONSTANTA,NR. 19-21, CLUJ-NAPOCA');
+INSERT INTO property(id,name,value) VALUES (4,'LOCATION_LATITUDE','46.775713');
+INSERT INTO property(id,name,value) VALUES (5,'LOCATION_LONGITUDE','23.594109');
+INSERT INTO property(id,name,value) VALUES (6,'EMAIL','bistro@yahoo.com');
+INSERT INTO property(id,name,value) VALUES (7,'RESTAURANT_NAME','BISTRO SRL');
+
